7a848ba4cd115220f1e7a6a13cadf75a544eae83
Rettye6 <renato.miranda.r@usach.cl>
2024-10-21 15:32:09 -0300

    Add files via upload

---
+++ 21999406-0.py
@@ -0,0 +1,35 @@
+# FUNDAMENTOS DE PROGRAMACIÓN PARA INGENIERÍA/FUNDAMENTOS DE COMPUTACIÓN Y PROGRAMACIÓN
+# SECCIÓN DEL CURSO: L-23
+# PROFESOR DE TEORÍA: Gabriel Godoy
+# PROFESOR DE LABORATORIO: Gery Gerena
+#
+# AUTOR
+# NOMBRE: Renato Miranda
+# RUN: 21.999.406-0
+# CARRERA: Inbgeniería Civil en Informática
+
+contra = input("Ingrese su pass: ")
+low = contra.lower()
+up = contra.upper()
+letras = "abcdefghijklmnopqrstuvwxyz"
+
+for contra in len[8]:
+    if contra in (low(letras)):
+        print("Debe tener al menos una minúscula.")
+    if contra in (up(letras)):
+        print("Debe tener al menos una mayúscula.")
+    if contra in ("letras"):
+        print("Debe tener al menos una letra.")
+    if contra in ("1,2,3,4,5,6,7,8,9,0"):
+        print("Debe tener al menos un dígito.")
+    if contra in (","):
+        print("Debe tener al menos una coma.")
+    if contra in ("." and ","):
+        print("Debe tener al menos una punto y coma")
+        
+    print("Su password cumple con todas las reglas.")
+
+    
+    
+    
+    
